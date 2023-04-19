d9a082d8a912ac854a4329c710182faeed703c67
Ilyar <kasyymov.ilyar@gmail.com>
2023-04-19 12:04:04 +0300

    added footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Container, Logo } from "@/components/ui";
+import styles from "./Footer.module.scss";
 
 export const Footer = () => {
 	return (
-		<footer>Footer</footer>
+		<footer className={styles.footer}>
+			<Container className={styles.wrapper}>
+				<Logo />
+				<a href="mailto:cinema.park.support@gmail.com">cinema.park.support@gmail.com</a>
+			</Container>
+		</footer>
 	);
 };
