f9b1535ddcba72ceaf479581cb6eedf13260dbae
Ilyar <kasyymov.ilyar@gmail.com>
2023-05-15 10:56:46 +0300

    some fixes

--- src/components/ui/logo/Logo.tsx
+++ src/components/ui/logo/Logo.tsx
@@ -1,28 +1,36 @@
-import { FC } from 'react';
+import { FC } from "react";
 import Link from "next/link";
 import cn from "classnames";
-import { useRouter } from 'next/router';
-import { SITE_NAME } from '../../constants';
+import { useRouter } from "next/router";
+import Image from "next/image";
+
 import { AppRoutes } from "@/components/constants/routes";
 import { LogoProps } from "./Logo.type";
+
 import styles from "./Logo.module.scss";
+import logoIcon from "@/assets/images/logo.png";
 
 export const Logo: FC<LogoProps> = ({ className }) => {
 	const { pathname } = useRouter();
-	//TODO поменять лого
+
+	const imageLayout = (
+		<Image
+			className={styles.image}
+			width={570}
+			height={100}
+			src={logoIcon.src}
+			alt="Логотип"
+			priority={true}
+		/>
+	);
 
 	if (pathname === AppRoutes.HOME) {
-		return <div className={cn(styles.logo, className)}>
-			{SITE_NAME}
-		</div>;
+		return <div className={cn(styles.logo, className)}>{imageLayout}</div>;
 	}
 
 	return (
-		<Link
-			href={AppRoutes.HOME}
-			className={cn(styles.logo, className)}
-		>
-			{SITE_NAME}
+		<Link href={AppRoutes.HOME} className={cn(styles.logo, className)}>
+			{imageLayout}
 		</Link>
 	);
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
-import type { AppProps } from 'next/app';
+import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Provider } from "react-redux";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 
 import { getScrollbarWidth, setCssVariable, throttle } from "@/helpers";
 import { wrapper } from "@/store/store";
 
 import "normalize.css";
-import '@/assets/styles/variables.scss';
-import '@/assets/styles/common.scss';
-import 'react-toastify/dist/ReactToastify.css';
+import "@/assets/styles/variables.scss";
+import "@/assets/styles/common.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = ({ Component, ...rest }: AppProps) => {
 	const { store, props } = wrapper.useWrappedStore(rest);
@@ -27,22 +27,37 @@ const App = ({ Component, ...rest }: AppProps) => {
 		return () => window.removeEventListener("resize", handleResize);
 	}, []);
 
-	return <>
-		<Provider store={store} >
-			<Head>
-				<title>
-					Фильмы смотреть онлайн, в хорошем HD 720 - 1080 качестве бесплатно
-				</title>
-				<meta name="description" content="Смотрите лучшие фильмы онлайн, в хорошем HD качестве совершенно бесплатно и без регистрации!" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Component {...props} />
-		</Provider>
-		<ToastContainer />
-	</>;
+	return (
+		<>
+			<Provider store={store}>
+				<Head>
+					<title>
+						Фильмы смотреть онлайн, в хорошем HD 720 - 1080 качестве бесплатно
+					</title>
+					<meta
+						name="description"
+						content="Смотрите лучшие фильмы онлайн, в хорошем HD качестве совершенно бесплатно и без регистрации!"
+					/>
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<link rel="icon" href="/assets/images/favicon.ico" />
+					<link
+						rel="icon"
+						type="image/png"
+						href="/assets/images/favicon-32x32.png"
+						sizes="32x32"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						href="/assets/images/favicon-16x16.png"
+						sizes="16x16"
+					/>
+				</Head>
+				<Component {...props} />
+			</Provider>
+			<ToastContainer />
+		</>
+	);
 };
 
 export default App;
-
-
